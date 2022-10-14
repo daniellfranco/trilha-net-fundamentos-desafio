57da0e14939c53a8ca98743ca0047d93d8dd0509
daniellfranco <daniel.luis.franco@gmail.com>
2022-10-14 18:05:22 -0300

    salvo primeiras alterações no codigo

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -17,6 +17,8 @@ namespace DesafioFundamentos.Models
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            //meu
+            veiculos.Add(Console.ReadLine());
         }
 
         public void RemoverVeiculo()
@@ -26,26 +28,51 @@ namespace DesafioFundamentos.Models
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
             string placa = "";
+            placa = Console.ReadLine();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+
 
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+
+                decimal valorTotal = 0;
+
+                valorTotal = ConfereECalculaHoras();
 
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
-
+                veiculos.Remove(placa);
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
             else
             {
                 Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
+        }
+
+        public decimal ConfereECalculaHoras()
+        {
+            int horas = 0;
+            Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+            int.TryParse(Console.ReadLine(), out horas);
+            if (horas < 0)
+            {
+                Console.WriteLine("Valor de horas inválido! O valor tem que ser maior ou igual a zero.");
+                return ConfereECalculaHoras();
+            }
+            Console.WriteLine($"O numero de horas digitado é {horas}, confirma? digite S para continuar ou qualquer letra para repetir");
+            string sn = Console.ReadLine();
+            if (sn == "S" || sn == "s")
+            {
+                return precoInicial + precoPorHora * horas;
+            }
+            else
+            {
+                return ConfereECalculaHoras();
             }
         }
 
